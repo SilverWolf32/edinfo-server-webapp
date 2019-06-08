9f0b93fd5147edba169b616b625066063c8ac846
SilverWolf <43831298+SilverWolf32@users.noreply.github.com>
2019-06-07 21:30:02 -0400

    Tell server to regenerate the HUD filter on page reload

--- colormatrix.js
+++ colormatrix.js
@@ -1,13 +1,15 @@
 // get the color matrix
 async function useHUDColor() {
-	// let toChange = document.getElementById("current-location")
-	let toChange = document.body
-	toChange.style = "filter: url(\"hudcolorfilter.svg#HUD\");"
 	// tell the server to regenerate the filter SVG file
 	console.log("Telling server to regenerate HUD color filter...")
-	socketio.emit("regenHUDfilter", "foo", function(result) {
-		console.log("Got result:", result)
+	fetch("http://localhost:3000/api/regenerate-hud-filter").then(function(response) {
+		console.log("Server has regenerated filter.")
 		
+		// let toChange = document.getElementById("current-location")
+		let toChange = document.body
+		toChange.style = "filter: url(\"hudcolorfilter.svg#HUD\");"
+	}).catch(function(error) {
+		console.log("Error regenerating HUD filter on server:", error)
 	})
 }
 
