b1b69243977ff9f44c5783b6846672648241850c
SilverWolf <43831298+SilverWolf32@users.noreply.github.com>
2019-06-07 22:26:11 -0400

    Don't regenerate the HUD filter file on every page load
    It's incredibly inefficient and generally unnecessary.

--- colormatrix.js
+++ colormatrix.js
@@ -1,15 +1,9 @@
 // get the color matrix
 async function useHUDColor() {
-	// tell the server to regenerate the filter SVG file
-	console.log("Telling server to regenerate HUD color filter...")
-	fetch("/api/regenerate-hud-filter").then(function(response) {
-		console.log("Server has regenerated filter.")
-		
-		// let toChange = document.getElementById("current-location")
-		let toChange = document.body
-		toChange.style = "filter: url(\"hudcolorfilter.svg#HUD\");"
-	}).catch(function(error) {
-		console.log("Error regenerating HUD filter on server:", error)
-	})
+	// let toChange = document.getElementById("current-location")
+	let toChange = document.body
+	toChange.style = "filter: url(\"hudcolorfilter.svg#HUD\");"
 }
+
+useHUDColor()
 
