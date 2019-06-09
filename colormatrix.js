6c9720bd8e919d6ad876d10b9ba9300fd9d0333a
SilverWolf <43831298+SilverWolf32@users.noreply.github.com>
2019-06-09 16:14:20 -0400

    Fix HUD color filter for Safari!
    Apparently Safari wants it to be embedded in the HTML.
    This also removes the need for the server to write the filter SVG to disk.

--- colormatrix.js
+++ colormatrix.js
@@ -1,8 +1,36 @@
 // get the color matrix
 async function useHUDColor() {
 	// let toChange = document.getElementById("current-location")
-	let toChange = document.body
-	toChange.style = "filter: url(\"hudcolorfilter.svg#HUD\");"
+	// let toChange = document.body
+	// toChange.style = "filter: url(\"hudcolorfilter.svg#HUD\");"
+	
+	fetch("/api/hudcolorfilter.svg")
+	.then(function(response) {
+		if (response.status != 200) {
+			// it's an error
+			return Promise.reject(response)
+		}
+		return response.text()
+	})
+	.then(function(svg) {
+		console.log("SVG:", svg)
+		// let container = document.getElementById("svgContainer")
+		// container.innerHTML = svg
+		
+		document.body.innerHTML += svg
+		
+		// see https://stackoverflow.com/a/24109000
+		// let parser = new DOMParser()
+		// let element = parser.parseFromString(svg, "image/svg+xml")
+		// document.querySelector("body").appendChild(element)
+	})
+	.then(function() {
+		let toChange = document.body
+		toChange.style = "filter: url(\"#HUD\");"
+	})
+	.catch(function(error) {
+		console.log(error)
+	})
 }
 
 useHUDColor()
